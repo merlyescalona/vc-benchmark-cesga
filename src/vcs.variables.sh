1b1389b8d5ca102ab519e0a5be218caba5173694
Merly Mayela Escalona Fermin <uvibemef@fs6801.local>
2017-09-26 12:18:35 +0200

    modified location of variable file

---
+++ src/vcs.variables.sh
@@ -0,0 +1,12 @@
+WD="$HOME/vc-benchmark-cesga"
+folderJOBS="$WD/jobs"
+folderDATA="$LUSTRE/data"
+folderOUTPUT="$LUSTRE/output"
+folderERROR="$LUSTRE/error"
+folderINFO="$LUSTRE/info"
+fileJOBS="$folderINFO/jobs.sent.txt"
+################################################################################
+# Variables
+################################################################################
+pipelinesName="vcs"
+MYRANDOMSEED=523911721 # echo "$RANDOM$RANDOM"ve
