ff9280bfb353e3435e802056a088c2cba7473e1e
Merly Escalona <escalona10@gmail.com>
2017-09-29 14:24:41 +0200

    added indelible single jobs

--- jobs/vcs.4.ngsphy.sh
+++ jobs/vcs.4.ngsphy.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 #SBATCH -n 1
-#SBATCH -t 00:30:00
+#SBATCH -t 12:00:00
 #
-#SBATCH --job-name=vcs.2
-#SBATCH --output=/mnt/lustre/scratch/home/uvi/be/mef/output/vcs.2.o
-#SBATCH --error=/mnt/lustre/scratch/home/uvi/be/mef/error/vcs.2.e
+#SBATCH --job-name=ngsphy
+#SBATCH --output=/mnt/lustre/scratch/home/uvi/be/mef/output/vcs.4.o
+#SBATCH --error=/mnt/lustre/scratch/home/uvi/be/mef/error/vcs.4.e
 #SBATCH --workdir=/mnt/lustre/scratch/home/uvi/be/mef/data/
 #
 #SBATCH --mail-type=begin,end
 #SBATCH --mail-user=escalona10@gmail.com
 #SBATCH --partition shared
 #SBATCH --qos=shared
-
-pipeID=$(printf "%05g" ${SLURM_ARRAY_TASK_ID})
+################################################################################
+# Variables
+replicateID=$(printf "%05g" ${SLURM_ARRAY_TASK_ID})
 pipelinesName="vcs"
-echo $pipeID, $pipelinesName
+simphyFOLDER=${pipelinesName}.$(printf "%05g" ${SLURM_ARRAY_TASK_ID})
+echo $(hostname),$pipelinesName,${SLURM_ARRAY_TASK_ID},$replicateID, $simphyFOLDER
+################################################################################
+# generate ngsphy settings file
+echo "generate ngsphy settings file"
+if [ ! -d $LUSTRE/data/ngsphy.settings ];then
+    mkdir $LUSTRE/data/ngsphy.settings
+fi
+################################################################################
+# PATHS
+ngsphyCOMMAND="$HOME/src/ngsphy/scripts/ngsphy"
+ngsphySettingsGenerator="$HOME/vc-benchmark-cesga/src/vcs.write.ngsphy"
+folderNGSPROFILE="$HOME/vc-benchmark-cesga/files"
+
+################################################################################
+$ngsphySettingsGenerator $pipelinesName $replicateID $LUSTRE/data/$simphyFOLDER
+################################################################################
 module purge
-module load gcc/6.3.0 os-devel/usr openssl/1.0.2f  gsl/2.3 perl/5.24.0
+module load anaconda2/4.0.0 gcc/5.3.0 art/2016-06-05
+################################################################################
 wrapper="$HOME/vc-benchmark-cesga/src/INDELible_wrapper_v2.pl"
 controlFile="$HOME/vc-benchmark-cesga/files/indelible.control.v2.txt"
 #Usage: ./INDELIble_wrapper.pl directory input_config seed numberofcores
 echo "perl $wrapper $pipelinesName.$pipeID $controlFile 523911721 1 &> \"$LUSTRE/output/$pipelinesName.$pipeID.1.2.indelible.wrapper.txt\""
 perl $wrapper $LUSTRE/data/$pipelinesName.$pipeID $controlFile 523911721 1 &> "$LUSTRE/output/$pipelinesName.$pipeID.1.2.indelible.wrapper.txt"
-module unload gcc/6.3.0 os-devel/usr openssl/1.0.2f  gsl/2.3 perl/5.24.0
+################################################################################
+module unload anaconda2/4.0.0 gcc/5.3.0 art/2016-06-05
