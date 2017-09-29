157b554ced7491fc8a71030b9142b332a6c94dcf
Merly Escalona <escalona10@gmail.com>
2017-09-29 10:30:43 +0200

    added ngs profiles for conus , script to generate ngsphy settings and job 4 ngsphy

---
+++ jobs/vcs.4.ngsphy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -t 00:30:00
+#
+#SBATCH --job-name=vcs.2
+#SBATCH --output=/mnt/lustre/scratch/home/uvi/be/mef/output/vcs.2.o
+#SBATCH --error=/mnt/lustre/scratch/home/uvi/be/mef/error/vcs.2.e
+#SBATCH --workdir=/mnt/lustre/scratch/home/uvi/be/mef/data/
+#
+#SBATCH --mail-type=begin,end
+#SBATCH --mail-user=escalona10@gmail.com
+#SBATCH --partition shared
+#SBATCH --qos=shared
+
+pipeID=$(printf "%05g" ${SLURM_ARRAY_TASK_ID})
+pipelinesName="vcs"
+echo $pipeID, $pipelinesName
+module purge
+module load gcc/6.3.0 os-devel/usr openssl/1.0.2f  gsl/2.3 perl/5.24.0
+wrapper="$HOME/vc-benchmark-cesga/src/INDELible_wrapper_v2.pl"
+controlFile="$HOME/vc-benchmark-cesga/files/indelible.control.v2.txt"
+#Usage: ./INDELIble_wrapper.pl directory input_config seed numberofcores
+echo "perl $wrapper $pipelinesName.$pipeID $controlFile 523911721 1 &> \"$LUSTRE/output/$pipelinesName.$pipeID.1.2.indelible.wrapper.txt\""
+perl $wrapper $LUSTRE/data/$pipelinesName.$pipeID $controlFile 523911721 1 &> "$LUSTRE/output/$pipelinesName.$pipeID.1.2.indelible.wrapper.txt"
+module unload gcc/6.3.0 os-devel/usr openssl/1.0.2f  gsl/2.3 perl/5.24.0
---
+++ src/vcs.write.ngsphy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+pipelinesName=$1
+idREPLICATE=$2
+folderSIMPHY=$3
+folderNGSPROFILE=$4
+fileOUTPUT=$5
+folderOUTPUT=$6
+echo -e "
+[general]
+path=${folderOUTPUT}
+output_folder_name=NGSphy_${pipelinesName}.${idREPLICATE}
+ploidy=2
+[data]
+inputmode=4
+simphy_folder_path=$folderSIMPHY
+simphy_data_prefix=vcs
+simphy_filter=true
+[coverage]
+experiment=F:100
+individual=LN:1.2,1
+locus=LN:1.2,1
+[ngs-reads-art]
+qprof1=${folderNGSPROFILE}/csNGSProfile_hiseq2500_1.txt
+qprof2=${folderNGSPROFILE}/csNGSProfile_hiseq2500_2.txt
+l=150
+m=215
+s=50
+q=true
+na=false
+ss=HS25
+[execution]
+environment=bash
+runART=off
+running_times=off
+threads=2
+" > $fileOUTPUT
