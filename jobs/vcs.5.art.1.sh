2af6eefdb6f358be66c8cee918e723da410ea118
Merly Escalona <escalona10@gmail.com>
2017-10-04 21:09:37 +0200

    updated art script

--- jobs/vcs.5.art.1.sh
+++ jobs/vcs.5.art.1.sh
@@ -3,8 +3,8 @@
 #SBATCH -t 1:00:00
 #
 #SBATCH --job-name=art
-#SBATCH --output=/mnt/lustre/scratch/home/uvi/be/mef/output/vcs.5.1.%a.o
-#SBATCH --error=/mnt/lustre/scratch/home/uvi/be/mef/error/vcs.5.1.%a.e
+#SBATCH --output=/mnt/lustre/scratch/home/uvi/be/mef/output/vcs.5.1.o
+#SBATCH --error=/mnt/lustre/scratch/home/uvi/be/mef/error/vcs.5.1.e
 #SBATCH --workdir=/mnt/lustre/scratch/home/uvi/be/mef/data/
 #
 #SBATCH --mail-type=end
@@ -16,5 +16,11 @@ echo -e "[$(date)]\nDefinition"
 command=$(awk "NR==${SLURM_ARRAY_TASK_ID}" $LUSTRE/data/ngsphy.data/NGSphy_vcs.00001/scripts/vcs.00001.sh)
 
 module load gcc/5.3.0 art/2016-06-05
+commandArray=($command)
+directory=$(dirname ${commandArray[-1]})
+echo -e "create dir\t:$directory"
+mkdir -p $directory
+echo $command
 $command
+
 module unload gcc/5.3.0 art/2016-06-05
