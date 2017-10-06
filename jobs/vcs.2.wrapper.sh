ca6cca71e13b264929d5985d1e7a1d06bd84c2b6
Merly Escalona <escalona10@gmail.com>
2017-10-06 15:57:32 +0200

    update pipelines name step2

--- jobs/vcs.2.wrapper.sh
+++ jobs/vcs.2.wrapper.sh
@@ -2,9 +2,9 @@
 #SBATCH -n 1
 #SBATCH -t 00:30:00
 #
-#SBATCH --job-name=vcs.2
-#SBATCH --output=/mnt/lustre/scratch/home/uvi/be/mef/output/vcs.2.o
-#SBATCH --error=/mnt/lustre/scratch/home/uvi/be/mef/error/vcs.2.e
+#SBATCH --job-name=ssp.2
+#SBATCH --output=/mnt/lustre/scratch/home/uvi/be/mef/output/ssp.2.o
+#SBATCH --error=/mnt/lustre/scratch/home/uvi/be/mef/error/ssp.2.e
 #SBATCH --workdir=/mnt/lustre/scratch/home/uvi/be/mef/data/
 #
 #SBATCH --mail-type=begin,end
@@ -13,12 +13,12 @@
 #SBATCH --qos=shared
 
 pipeID=$(printf "%05g" ${SLURM_ARRAY_TASK_ID})
-pipelinesName="vcs"
+pipelinesName="ssp"
 echo $pipeID, $pipelinesName
 module purge
 module load gcc/6.3.0 os-devel/usr openssl/1.0.2f  gsl/2.3 perl/5.24.0
 wrapper="$HOME/vc-benchmark-cesga/src/INDELible_wrapper_v2.pl"
-controlFile="$HOME/vc-benchmark-cesga/files/indelible.control.v2.txt"
+controlFile="$HOME/vc-benchmark-cesga/files/indelible.control.txt"
 #Usage: ./INDELIble_wrapper.pl directory input_config seed numberofcores
 echo "perl $wrapper $pipelinesName.$pipeID $controlFile 523911721 1 &> \"$LUSTRE/output/$pipelinesName.$pipeID.1.2.indelible.wrapper.txt\""
 perl $wrapper $LUSTRE/data/$pipelinesName.$pipeID $controlFile 523911721 1 &> "$LUSTRE/output/$pipelinesName.$pipeID.1.2.indelible.wrapper.txt"
