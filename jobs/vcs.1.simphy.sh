2b2f503c8f912b3e801f342d09d3e490d8788e11
Merly Escalona <escalona10@gmail.com>
2017-10-14 19:08:04 +0200

    updated automation

--- jobs/vcs.1.simphy.sh
+++ jobs/vcs.1.simphy.sh
@@ -31,7 +31,7 @@ HH="L:1.2,1" # Gene-by-family heterogeneity
 HG="F:GP" # Gene-by-lineage-specific rate heterogeneity modifier
 
 module purge
-module load gcc/5.3.0 simphy/1.0.2 R/3.2.4
+module load gcc/5.3.0 simphy/1.0.2 R/3.3.3
 simphy  -rs $RS -rl $RL -su $SU -sb $SB -sl $SL -si $SI -sp $SP -st $ST -so $SO \
         -sg $SG -gp $GP -hh $HH -hg $HG  -v 1 \
         -o ${pipelinesName}.$(printf "%05g" ${SLURM_ARRAY_TASK_ID}) \
@@ -43,6 +43,9 @@ Rscript $HOME/vc-benchmark-cesga/src/filtering.even.R \
     "$HOME/vc-benchmark-cesga/files/${pipelinesName}.$(printf "%05g" ${SLURM_ARRAY_TASK_ID}).evens" \
     "$HOME/vc-benchmark-cesga/files/${pipelinesName}.$(printf "%05g" ${SLURM_ARRAY_TASK_ID}).odds"
 
-find $LUSTRE/data/${pipelinesName}.$(printf "%05g" ${SLURM_ARRAY_TASK_ID}) -mindepth 1 -maxdepth 1 -type d | sort > $HOME/vc-benchmark-cesga/files/${pipelinesName}.$(printf "%05g" ${SLURM_ARRAY_TASK_ID}).indelible.folders.txt
+for line in $(cat $HOME/vc-benchmark-cesga/files/${pipelinesName}.$(printf "%05g" ${SLURM_ARRAY_TASK_ID}).evens); do
+  st=$(printf "%02g" ${line})
+  echo "$LUSTRE/data/${pipelinesName}.$(printf "%05g" ${SLURM_ARRAY_TASK_ID})/${st}" >> $HOME/vc-benchmark-cesga/files/${pipelinesName}.$(printf "%05g" ${SLURM_ARRAY_TASK_ID}).indelible.folders.txt
+done
 
-module unload gcc/5.3.0 simphy/1.0.2 R/3.2.4
+module unload gcc/5.3.0 simphy/1.0.2 R/3.3.3
