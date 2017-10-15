b140831c45f89dc4139da5803a8b349f7a389298
Merly Escalona <escalona10@gmail.com>
2017-10-15 19:22:14 +0200

    split array command for art

---
+++ jobs/vcs.5.art.split.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -t 12:00:00
+#
+#SBATCH --job-name=art
+#SBATCH --output=/mnt/lustre/scratch/home/uvi/be/mef/output/ssp.5.00001.%a.o
+#SBATCH --error=/mnt/lustre/scratch/home/uvi/be/mef/error/ssp.5.00001.%a.e
+#SBATCH --workdir=/mnt/lustre/scratch/home/uvi/be/mef/data/
+#
+#SBATCH --partition shared
+#SBATCH --qos=shared
+
+echo -e "[$(date)]\nDefinition"
+command=$(awk "NR==${SLURM_ARRAY_TASK_ID}")
+nlines=$(wc -l $1 | awk '{print $1}')
+module load gcc/5.3.0 art/2016-06-05
+for item in $(seq 1 $nlines);do
+    echo "$item"
+    command=$(awk -v x=$item "NR==x")
+    $command
+done
+
+module unload gcc/5.3.0 art/2016-06-05
