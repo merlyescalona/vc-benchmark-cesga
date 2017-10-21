6bfef47621a1bbef2c0865878a719643331833b1
Merly Escalona <escalona10@gmail.com>
2017-10-21 08:53:03 +0200

    updated jobs for bwa

--- jobs/analysis/ssp.analysis.3.sh
+++ jobs/analysis/ssp.analysis.3.sh
@@ -9,7 +9,7 @@ module load gcc/5.2.0 bio/bwa/0.7.10
 ################################################################################
 correctID=$SGE_TASK_ID
 let correctID=correctID-1
-filename="$1.$(printf "%02g" $correctID).sh"
+filename="$1.$(printf "%03g" $correctID).sh"
 nlines=$(wc -l $filename | awk '{print $1}')
 for item in $(seq 1 $nlines);do
     echo "$item"
