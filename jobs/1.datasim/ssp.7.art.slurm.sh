c47dd9d953eed16afa421453d63f5733cf279e76
Merly Escalona <escalona10@gmail.com>
2017-10-24 12:47:09 +0200

    updated notifications job art slurm

--- jobs/1.datasim/ssp.7.art.slurm.sh
+++ jobs/1.datasim/ssp.7.art.slurm.sh
@@ -7,6 +7,8 @@
 #SBATCH --error=/mnt/lustre/scratch/home/uvi/be/mef/error/art.7.%a.e
 #SBATCH --workdir=/mnt/lustre/scratch/home/uvi/be/mef/data/
 #
+#SBATCH --mail-type=end
+#SBATCH --mail-user=escalona10@gmail.com
 #SBATCH --partition shared,gpu-shared-k2
 #SBATCH --qos=shared
 
