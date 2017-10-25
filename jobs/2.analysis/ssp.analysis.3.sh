2977a93c4fe98ecc3269656eb02c221b8317c5c3
Merly Escalona <escalona10@gmail.com>
2017-10-25 13:20:07 +0200

    Updated digits for launching bwa commands

--- jobs/2.analysis/ssp.analysis.3.sh
+++ jobs/2.analysis/ssp.analysis.3.sh
@@ -9,6 +9,6 @@ module load gcc/5.2.0 bio/bwa/0.7.10
 ################################################################################
 correctID=$SGE_TASK_ID
 let correctID=correctID-1
-filename="$1.$(printf "%03g" $correctID).sh"
+filename="$1.$(printf "%05g" $correctID).sh"
 bash $filename
 module unload  gcc/5.2.0 bio/bwa/0.7.10
